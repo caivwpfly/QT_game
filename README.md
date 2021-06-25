aebdc2759a39594039d48d9092c7b95a8177e213
yuanzai <caivwpfly@163.com>
2021-06-25 22:55:36 +0800

    first qt

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # QT_game
 first test for qt
+## 编译脚本
+qmake -project
+qmake QT_game
+make
+./release/QT_game
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<QApplication>
+#include<QWidget>
+
+int main(int argc,char ** argv)
+{
+    QApplication app(argc,argv);
+    QWidget* pWidget = new QWidget();
+    pWidget->show();
+    return app.exec();
+
+}
